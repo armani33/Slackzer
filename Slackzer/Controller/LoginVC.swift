8a9c7a41a4fd0d03fe4946d90fb14dd424f77f6f
armand chariot <armand@chariot.be>
2019-06-24 20:42:41 +0200

    add LoginVC and style the view.

---
+++ Slackzer/Controller/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  Slackzer
+//
+//  Created by Armand Chariot on 24/06/2019.
+//  Copyright © 2019 Armand Chariot. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Slackzer/Utilities/constants.swift
@@ -0,0 +1,13 @@
+//
+//  constants.swift
+//  Slackzer
+//
+//  Created by Armand Chariot on 24/06/2019.
+//  Copyright © 2019 Armand Chariot. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
