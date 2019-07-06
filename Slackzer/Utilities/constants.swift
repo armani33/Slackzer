103b3e3711e38925bba144df609fa9c36696d974
armand chariot <armand@chariot.be>
2019-07-06 17:24:26 +0200

    Login doesn't work don't understand - fuuuckkk i'm bored 2 days trying to find

--- Slackzer/Utilities/constants.swift
+++ Slackzer/Utilities/constants.swift
@@ -14,8 +14,9 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 
 let BASE_URL = "https://slackzer.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
-let URL_LOGIN = "\(BASE_URL)/account/login"
-let URL_USER_ADD = "\(BASE_URL)/user/add"
+let URL_LOGIN = "\(BASE_URL)account/login"
+let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_USER_BY_EMAIL = "\(BASE_URL)/user/byEmail/"
 
 //Segues
 
@@ -37,9 +38,13 @@ let HEADER = [
     "Content-Type": "application/json; charset=utf-8"
 ]
 
+let BEARER_HEADER = [
+    "Authorization": "Bearer \(AuthService.instance.authToken)",
+    "Content-Type": "application/json; charset=utf-8"
+]
+
 //Colors
 let slackzerPurplePlaceholder = #colorLiteral(red: 0.2588235294, green: 0.3294117647, blue: 0.7254901961, alpha: 0.5)
 
 //Notification Constants
-
 let NOTIF_USER_DATA_DID_CHANGE = Notification.Name("notifUserDataChanged")
